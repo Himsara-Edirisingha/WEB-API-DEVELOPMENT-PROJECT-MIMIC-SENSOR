e05d774026bf15dc8a0bcf94857ed1a4b3e77fd6
Himsara-Edirisingha <87166862+Himsara-Edirisingha@users.noreply.github.com>
2024-03-26 18:16:49 +0530

    mimic without encryption and exception hanndle

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 const http = require('http');
 
-const auth = () => {
+let authkey = "";
 
+function setauthkey(key) {
+    authkey = key;
+    return true;
+}
+
+async function getauthkey() {
+    return authkey;
+}
+
+const auth = () => {
     const postData = JSON.stringify({
-      id:process.env.DEVICE_ID
+        id: process.env.DEVICE_ID
     });
 
     const options = {
@@ -31,6 +41,8 @@ const auth = () => {
         res.on('end', () => {
             const responseData = Buffer.concat(data).toString();
             console.log('Response Data:', JSON.parse(responseData));
+            setauthkey(JSON.parse(responseData).token);
+            setInterval(write, process.env.DATA_INTERVAL); // Start interval after authentication
         });
     });
 
@@ -40,6 +52,53 @@ const auth = () => {
 
     req.write(postData);
     req.end();
-}
+};
 
-auth();
+const write = () => {
+    const postData = JSON.stringify({
+        id: 5,
+        name: "Item 5"
+    });
+
+    const options = {
+        hostname: process.env.SERVER_HOST,
+        port: process.env.SERVER_PORT,
+        path: '/api/metrics/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData),
+            'authorization': "Bearer " + authkey
+        },
+    };
+
+    const req = http.request(options, res => {
+        let data = [];
+        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+        console.log('Status Code:', res.statusCode);
+        console.log('Date in Response header:', headerDate);
+
+        res.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        res.on('end', () => {
+            const responseData = Buffer.concat(data).toString();
+            console.log('Response Data:', JSON.parse(responseData));
+        });
+    });
+
+    req.on('error', err => {
+        console.error('Error:', err.message);
+    });
+
+    req.write(postData);
+    req.end();
+};
+
+if (authkey == "") {
+    auth();
+} else {
+    console.log("writing...")
+    setInterval(write, process.env.DATA_INTERVAL);
+}
